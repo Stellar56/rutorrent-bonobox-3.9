f07eae3fb263b19dccf799b88af521424c464ca4
Stellar56 <64519184+Stellar56@users.noreply.github.com>
2020-04-29 09:58:02 +0200

    Add files via upload

--- files/scripts/check-rtorrent.sh
+++ files/scripts/check-rtorrent.sh
@@ -6,7 +6,7 @@
 
 
 # includes
-INCLUDES="/tmp/rutorrent-bonobox/includes"
+INCLUDES="/tmp/rutorrent-bonobox-3.9/includes"
 # shellcheck source=/dev/null
 . "$INCLUDES"/cmd.sh
 # shellcheck source=/dev/null
